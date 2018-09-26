6b89b88f21b6cc57b376098aeba3e91577b97df4
Christian Powell <christiant.powell@gmail.com>
2018-09-26 12:59:11 -0500

    Add form inputs

--- index.php
+++ index.php
@@ -10,12 +10,37 @@
 
 <body>
 <div class='container'>
-<img src='/images/nutro.png' alt='Dog Food' />
-<h1>Nutro Dog Food Calculator</h1>
+    <img src='/images/nutro.png' alt='Dog Food'/>
+    <h1>Nutro Dog Food Calculator</h1>
 
-<form method='Get' action='calculate.php'>
+    <form method='Get' action='calculate.php'>
+        <label>Your Pet's Name:
+            <input type='text' name='petName'>
+        </label>
+        <label for='size'>Select Your Pet's Size:</label>
+        <select name='size' id='size'>
+            <option value='choose'>Choose one...</option>
+            <option value='15'>Up to 15 lbs.</option>
+            <option value='25'>16-25lbs.</option>
+            <option value='60'>41-60lbs.</option>
+            <option value='80'>61-80lbs.</option>
+            <option value='100'>81-100lbs.</option>
+            <option value='101'>Over 100lbs.</option>
+        </select>
+        <label>Your Pet's Age:</label>
+            <ul>
+                <li>
+                    <label><input type='checkbox' name='petAge'> Puppy</label>
+                </li>
+                <li>
+                    <label><input type='checkbox' name='petAge'> Adult</label>
+                </li>
+                <li>
+                    <label><input type='checkbox' name='petAge'> Senior</label>
+                </li>
+            </ul>
 
-</form>
+    </form>
 
 </div>
 </body>
