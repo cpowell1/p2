0bffa8bea538a1d9e65a5aa9271fa23cd6c41617
Christian Powell <christiant.powell@gmail.com>
2018-09-26 12:37:39 -0500

    Add HTML structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <title>Nutro Dog Food Calculator</title>
+    <meta charset="utf-8">
+    <link href='style.css' rel='stylesheet'>
+
+</head>
+
+<body>
+<h1>Nutro Dog Food Calculator</h1>
+
+<form method='' action=''>
+
+</form>
+
+
+</body>
+</html>
